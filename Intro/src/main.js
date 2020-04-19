d0e4d9947dc2d7488180834adf074d01867c68c0
moytri <moytritopa@gmail.com>
2020-04-19 13:09:17 -0700

    creating component

--- Intro/src/main.js
+++ Intro/src/main.js
@@ -1,35 +1,104 @@
-// vue instance is the root/heart of the vue application
-// it is created by passing an options object into it
-// options is used to store data and perfrom operation
-// el is used to form a relation between Vue instance and part of the dom
-// Vue instance is reactive
-// if we update the value of the product, everywhere it is used will be updated
-var app = new Vue({
- el: '#app', 
- data: {
-    product: 'Socks',
-    brand: 'Vue Mastery',
-    description: 'It is a cotton Sock',
-    //image: './assets/vmSocks-green.jpg',
-    selectedVarient: 0,
-    link: 'https://www.amazon.com/s?k=socks&ref=nb_sb_noss',
-    inventory: 10,
-    details: ["80% cotton", "20% polyster", "Gender-neutral"],
-    varients: [
-        {
-            varientId: 2234,
-            varientColor: 'green',
-            varientImage: './assets/vmSocks-green.jpg'
-        },
-        {
-            varientId: 2235,
-            varientColor: 'blue',
-            varientImage: './assets/vmSocks-blue.jpg'
+
+Vue.component('product-deatils', {
+    props: {
+        details: {
+            type: Array,
+            required: true,
         }
-    ],
-    sizes: ["XXL", "XL", "L", "M", "S"],
-    cart: 0
- },
+    },
+    template: `<!--List Rendering v-for directive-->
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>`
+})
+
+Vue.component('product', {
+    props: {
+        premium:{
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `<div class="product">
+
+    <!-- Html Attribute: https://www.w3schools.com/html/html_attributes.asp
+    v-bind:dynamically bind an attribute to an expression
+    shorthand :src, :alt, :disabled, :href-->
+    <div class="product-image">
+        <!-- <img v-bind:src="image"> 
+        instead of using image and change it using updateProduct(varient.varientImage), we can change image using computed property-->
+        <img v-bind:src="image">
+    </div>
+
+    <div class="product-info">
+        <!-- {{ }} is the expression -->
+        <h1>{{ title }}</h1>
+        <h2>{{ description }}</h2>
+        <p>Shipping: {{ shipping }}</p>
+
+        <!-- Conditionally Rendering v-if and v-show directive
+        v-if is conditionally add and remove element in the dom
+        on contrary v-show is not removing and adding element in the dom
+        it is just toggoling between and hide and show element into dom-->
+        <p v-if="inventory > 10">In Stock</p>
+        <p v-else-if="inventory<= 10 && inventory > 0">Almost Sold</p>
+        <p v-else>Out of Stock</p>
+
+        <product-deatils :details="details"></product-deatils>
+
+        <!--Style Binding-->
+        <div v-for="(varient, index) in varients" 
+        :key="varient.varientId"
+        class="color-box"
+        :style="{ backgroundColor: varient.varientColor}"
+        v-on:mouseover="updateProduct(index)"> 
+        </div>
+                        
+        <p v-if="inventory > 10 || inventory <= 10 && inventory > 0">
+            Available Sizes:
+            <ul>
+                <li v-for="size in sizes">{{ size }}</li>
+            </ul>
+        </p>
+        <a v-bind:href="link" target="_blank">More products</a>
+        <br>
+
+        <!--Event Handling e.g: @click, v-on:mouseover, @submit, @keyup.enter-->
+        <!--Class Binding: https://codepen.io/GreggPollack/pen/YBmqaX-->
+        <button v-on:click="addToCart"
+        :class="{ disabledButton: inventory == 0 }">Add to Cart</button>
+        <button @click="decrementFromCart">Decrement</button>
+        <div class="cart">
+            <p>Cart({{cart}})</p>
+        </div>
+    </div>
+</div>`,
+data() {
+    return {
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        description: 'It is a cotton Sock',
+        //image: './assets/vmSocks-green.jpg',
+        selectedVarient: 0,
+        link: 'https://www.amazon.com/s?k=socks&ref=nb_sb_noss',
+        inventory: 10,
+        details: ["80% cotton", "20% polyster", "Gender-neutral"],
+        varients: [
+            {
+                varientId: 2234,
+                varientColor: 'green',
+                varientImage: './assets/vmSocks-green.jpg'
+            },
+            {
+                varientId: 2235,
+                varientColor: 'blue',
+                varientImage: './assets/vmSocks-blue.jpg'
+            }
+        ],
+        sizes: ["XXL", "XL", "L", "M", "S"],
+        cart: 0
+    }
+},
 
  methods: {
     addToCart: function() {
@@ -57,7 +126,22 @@ var app = new Vue({
     },
     image() {
         return this.varients[this.selectedVarient].varientImage;
+    },
+    shipping() {
+        return this.premium ? "Free" : "2.99"
     }
  }
+})
 
+// vue instance is the root/heart of the vue application
+// it is created by passing an options object into it
+// options is used to store data and perfrom operation
+// el is used to form a relation between Vue instance and part of the dom
+// Vue instance is reactive
+// if we update the value of the product, everywhere it is used will be updated
+var app = new Vue({
+ el: '#app',
+ data: {
+    premium: true
+ }
 })
